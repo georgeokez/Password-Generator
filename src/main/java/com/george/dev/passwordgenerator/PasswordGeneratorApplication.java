f12faa2568d229298d11b616dd53598e505468c3
george <gokereka@teamapt.com>
2018-11-16 21:35:28 +0100

    Run app on my local box. closes #2

--- src/main/java/com/george/dev/passwordgenerator/PasswordGeneratorApplication.java
+++ src/main/java/com/george/dev/passwordgenerator/PasswordGeneratorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PasswordGeneratorApplication {
 
+    //App Works well
+
     public static void main(String[] args) {
         SpringApplication.run(PasswordGeneratorApplication.class, args);
     }
